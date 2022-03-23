5e488c77af1bac7fb92bb52e4df5580e920b4837
Vincenzo Villanova <VLLVCN02M19C351R@studium.unict.it>
2022-03-23 09:24:57 +0100

    Init

---
+++ Fibonacci.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int Fib(int n){
+    if(n==0){
+        return 1;
+    }
+    if(n==1){
+        return 1;
+    }
+    return Fib(n-1)+Fib(n-2);
+}
+int main(){
+    cout<<"Fib 4: "<<Fib(4);
+}
